5204b91f164de5fba296b1baddcea501a74d297f
dmytroleskivUA <121440440+dmytroleskivUA@users.noreply.github.com>
2023-05-05 18:19:55 +0100

    all tasks

---
+++ src/js/task-06.js
@@ -0,0 +1,27 @@
+const input = document.querySelector("#validation-input");
+
+input.addEventListener("blur", () => {
+  const expectedLength = input.getAttribute("data-length");
+  const actualLength = input.value.length;
+
+  if (actualLength === Number(expectedLength)) {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+  }
+});
+
+// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+
+// <input
+//   type="text"
+//   id="validation-input"
+//   data-length="6"
+//   placeholder="Please enter 6 symbols"
+// />
+
+// Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+// Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const changeButton = document.querySelector("button.change-color");
+const colorValue = document.querySelector("span.color");
+
+changeButton.addEventListener("click", (e) => {
+  let newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorValue.textContent = newColor;
+});
+
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
