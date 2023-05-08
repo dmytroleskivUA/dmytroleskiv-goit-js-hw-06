5048846d99b5f13029d52e346d6a70092028c585
dmytroleskivUA <121440440+dmytroleskivUA@users.noreply.github.com>
2023-05-08 21:09:28 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -9,18 +9,11 @@ const ingredients = [
 
 const ingredientsList = document.querySelector("#ingredients");
 
-ingredients.forEach((ingredient) => {
+const ingredientItems = ingredients.map((ingredient) => {
   const ingredientItem = document.createElement("li");
   ingredientItem.textContent = ingredient;
   ingredientItem.classList.add("item");
-  ingredientsList.append(ingredientItem);
+  return ingredientItem;
 });
 
-console.log(ingredientsList);
-console.log(ingredients);
-
-// Скрипт
-// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
-// Додасть назву інгредієнта як його текстовий вміст.
-// Додасть елементу клас item.
-// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+ingredientsList.append(...ingredientItems);
